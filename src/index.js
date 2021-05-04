56e0a2aba8e848e149d01e2bcdaa359091df8d19
sidewndr <anoshkinzz@mail.ru>
2021-05-04 14:43:35 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const app = (
+  <>
+
+  </>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
