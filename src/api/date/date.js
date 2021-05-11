12c7114c44247432edaf8a9392e3420d881a3a20
sidewndr <anoshkinzz@mail.ru>
2021-05-11 22:06:37 +0200

    [ADD]: init

---
+++ src/api/date/date.js
@@ -0,0 +1,26 @@
+export const date = {
+
+  getDate: (currentDate = new Date()) => {
+  return(
+    currentDate.getDate() < 10
+      ? '0' + currentDate.getDate()
+      : currentDate.getDate()
+    )
+  }
+  ,
+
+  getMonth: (currentDate = new Date()) => (
+    currentDate.getMonth() < 10
+      ? '0' + (currentDate.getMonth() + 1)
+      : currentDate.getMonth() + 1
+  ),
+
+  getYear: (currentDate = new Date()) => (
+    currentDate.getFullYear()
+  ),
+
+  getDay: (currentDate = new Date()) => {
+    const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    return days[currentDate.getDay()]
+  }
+}
