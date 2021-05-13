3792065a65facc3c9aa64dc12cc4995038d43013
sidewndr <anoshkinzz@mail.ru>
2021-05-14 01:26:26 +0300

    [ADD]: db config init

---
+++ src/api/database/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import 'firebase/firestore'
+
+
+firebase.initializeApp(
+  {
+    apiKey: "AIzaSyB9777y8v5Tre6a7EaGLSUPFj7AJQYugKI",
+    authDomain: "schedule-b0e23.firebaseapp.com",
+    projectId: "schedule-b0e23",
+    storageBucket: "schedule-b0e23.appspot.com",
+    messagingSenderId: "915629147208",
+    appId: "1:915629147208:web:10db3fd50fcdf4c40ec47d",
+    measurementId: "G-Q00RER9K0K"
+  }
+);
